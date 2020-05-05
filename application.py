9da80b7b7301e72518fdd79064136dd2ba9692aa
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-05-05 17:05:59 +1000

    Create application.py

---
+++ application.py
@@ -0,0 +1,27 @@
+import requests
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/convert", methods=["POST"])
+def convert():
+    #Query for currency exchange rate
+    currency = request.form.get("currency")
+    res = requests.get("https://data.fixer.io/api/latest?access_key=YOUR_ACCESS_KEY:, params {"base": "USD", "symbols": currency})
+    
+    #Make sure request succeeded
+    if res.status_code != 200:
+        retur jsonify({"success": False})
+    
+    #Make sure currency is in response
+    data = res.json()
+    if currency not in data["rates"]:
+        return jsonify({"success": False})
+    
+    return jsonify({"success": True, "rate": data["rates"][currency]})
+    
